4e23930da2b48cbbfd485a22d74ea1ba85a26f0d
Iskandar Setiadi <iskandar.setiadi@hde.co.jp>
2017-10-19 16:18:50 +0900

    Improve translate tests

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -3,7 +3,7 @@ import os
 
 from mock import Mock, patch
 
-from maidchan.translate import get_trans_language_prediction
+from maidchan.translate import get_trans_language_prediction, get_translation
 
 SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,15 +14,39 @@ def _get_response(name):
         return f.read()
 
 
-class TestTranslate:
-    @patch('subprocess.Popen')
-    def test_get_translate_language_prediction(self, mock_subproc_open):
-        process_mock = Mock()
-        attrs = {'communicate.return_value': (
-            _get_response('get_trans_prediction.txt'), None
-        )}
-        process_mock.configure_mock(**attrs)
-        print(process_mock.communicate())
-        mock_subproc_open.return_value = process_mock
-        assert get_trans_language_prediction("Hello, World!") == "en"
+def mocked_trans(*args, **kwargs):
+    """
+    Mocked "trans"
+    """
+    process_mock = Mock()
+    return_value = None
+    if '-id' in args[0] and 'hello, world!' in args[0]:
+        return_value = _get_response('get_trans_prediction.txt')
+    elif '-b' in args[0] and 'en:ja' in args[0] and 'hello, world!' in args[0]:
+        return_value = _get_response('get_trans_translation.txt')
+    elif '-b' in args[0] and 'en:id' in args[0] and 'hello, world!' in args[0]:
+        return_value = _get_response('get_trans_translation_2.txt')
+    attrs = {'communicate.return_value': (return_value, None)}
+    process_mock.configure_mock(**attrs)
+    return process_mock
 
+
+class TestTranslate:
+    @patch('subprocess.Popen', side_effect=mocked_trans)
+    def test_get_translate_language_prediction(self, mock_trans):
+        assert get_trans_language_prediction("hello, world!") == "en"
+
+    @patch('subprocess.Popen', side_effect=mocked_trans)
+    def test_get_translation_en_to_ja(self, mock_trans):
+        query = "translate hello, world! from english to japanese"
+        assert get_translation(query).decode('utf-8') == u"こんにちは世界！"
+
+    @patch('subprocess.Popen', side_effect=mocked_trans)
+    def test_get_translation_en_to_default(self, mock_trans):
+        query = "translate hello, world! from english"
+        assert get_translation(query).decode('utf-8') == u"こんにちは世界！"
+
+    @patch('subprocess.Popen', side_effect=mocked_trans)
+    def test_get_translation_default_to_id(self, mock_trans):
+        query = "translate hello, world! to bahasa"
+        assert get_translation(query).decode('utf-8') == "Halo Dunia!"
